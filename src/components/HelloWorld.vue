40d9be87882e1008cb356db4e86c411ae405deeb
adyaruan00 <adydaniel2000@gmail.com>
2023-03-20 10:15:20 +0700

    New Update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>Simple DevOps Project, Deploy Static Web App (Vue) using Github & Netlify </h1>
+    <h2>This is the new update for CI/CD</h2>
   </div>
 </template>
 
@@ -18,14 +19,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
